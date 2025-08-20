7bb1e12658d6fba6539196c2ca880b34d59d8f4f
dkhedekar79 <57810277+dkhedekar79@users.noreply.github.com>
2025-08-20 14:41:31 +0100

    Update Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -175,7 +175,7 @@ const Landing = () => {
       {/* Navigation Bar */}
       <header class="pb-6 bg-white lg:pb-0">
         <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <!-- lg+ -->
+            
             <nav class="flex items-center justify-between h-16 lg:h-20">
                 <div class="flex-shrink-0">
                     <a href="#" title="" class="flex">
@@ -184,12 +184,12 @@ const Landing = () => {
                 </div>
       
                 <button type="button" class="inline-flex p-2 text-black transition-all duration-200 rounded-md lg:hidden focus:bg-gray-100 hover:bg-gray-100">
-                    <!-- Menu open: "hidden", Menu closed: "block" -->
+                    
                     <svg class="block w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h16M4 16h16" />
                     </svg>
       
-                    <!-- Menu open: "block", Menu closed: "hidden" -->
+                   
                     <svg class="hidden w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
@@ -208,7 +208,7 @@ const Landing = () => {
                 <a href="#" title="" class="items-center justify-center hidden px-4 py-3 ml-10 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md lg:inline-flex hover:bg-blue-700 focus:bg-blue-700" role="button"> Get started now </a>
             </nav>
       
-            <!-- xs to lg -->
+            
             <nav class="pt-4 pb-6 bg-white border border-gray-200 rounded-md shadow-md lg:hidden">
                 <div class="flow-root">
                     <div class="flex flex-col px-6 -my-2 space-y-1">
