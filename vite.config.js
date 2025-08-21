eaa1023efd91939e57312cf3f2517f6a352b7d0e
dkhedekar79 <57810277+dkhedekar79@users.noreply.github.com>
2025-08-21 11:52:22 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,18 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true,
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600,
   },
-  publicDir: "public"
+  publicDir: 'public',
 });
+
